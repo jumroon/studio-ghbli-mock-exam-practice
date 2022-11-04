3cfbf01c542b34869192015711ae9b0cc18696e0
jumroon <jumroon.rada@gmail.com>
2022-11-04 16:41:36 +0700

    first commmit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+
+export default function App() {
+  console.log("RENDERING");
+
+  const filmUrl = "https://ghibliapi.herokuapp.com/films";
+  const [films, setFilms] = useState([]);
+  const [film, setFilm] = useState({});
+
+  useEffect(() => {
+    async function loadFilms() {
+      try {
+        const response = await fetch(filmUrl);
+        const someFilms = await response.json();
+        setFilms(someFilms);
+        console.log("response", someFilms);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    loadFilms();
+  }, []);
+
+  // function onClickHandler(filmImage, filmTitle) {
+  //   setImage(filmImage);
+  //   setTitle(filmTitle);
+  // }
+
+  function onClickHandler(film) {
+    setFilm(film);
+  }
+
+  const filmTitles = films.map((film, index) => {
+    const onClickWithMovie = () => onClickHandler(film);
+
+    return (
+      <p key={index}>
+        <button onClick={onClickWithMovie}>{film.title}</button>
+      </p>
+    );
+  });
+
+  const filmTitlesArray = films.map((film) => film.title);
+
+  console.log("FILM TITLES", filmTitlesArray);
+  return (
+    <div className="App">
+      <h1>Film Titles</h1>
+      <h2>{filmTitles}</h2>
+      <img src={film.image} title={film.title}></img>
+    </div>
+  );
+}
